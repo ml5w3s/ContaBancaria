56a5e1a5f39000fdcd3b832e86e21c8e666bfb28
ML5W3S <marcelolauriano@gmail.com>
2022-10-17 17:34:38 -0300

    Add files via upload

---
+++ src/contasbancarias/BEANS/ClienteConta.java
@@ -0,0 +1,29 @@
+package BEANS;
+
+public class ClienteConta {
+        private String clienteConta, valorDeclarado, numConta;
+
+    public String getClienteConta() {
+        return clienteConta;
+    }
+
+    public void setClienteConta(String clienteConta) {
+        this.clienteConta = clienteConta;
+    }
+
+    public String getValorDeclarado() {
+        return valorDeclarado;
+    }
+
+    public void setValorDeclarado(String valorDeclarado) {
+        this.valorDeclarado = valorDeclarado;
+    }
+
+    public String getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(String numConta) {
+        this.numConta = numConta;
+    }
+}
