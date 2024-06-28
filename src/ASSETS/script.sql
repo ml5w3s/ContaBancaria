5a54c33dcc80e8b537ae9de722961fc8b35c6181
ml5w3s <marcelo.lauriano@gmail.com>
2024-06-28 18:05:15 -0300

    Refector add colunas na tabela cliente

--- src/ASSETS/script.sql
+++ src/ASSETS/script.sql
@@ -9,7 +9,9 @@ CREATE TABLE bairro(
 CREATE TABLE cliente(
    id_cliente INT NOT NULL auto_increment,
    nome_cliente VARCHAR(45) NOT NULL,
-   fk_bairro_id VARCHAR(45) NOT NULL,
+   num_conta VARCHAR (45),
+   senha INT NOT NULL,
+   fk_bairro_id INT NOT NULL,
    PRIMARY KEY(id_cliente),
    FOREING KEY (fk_bairro_id) REFERENCES bairro(id_bairro)
    ) ENGINE = innoDB DEFAULT CHARSET=utf8;
@@ -17,7 +19,7 @@ CREATE TABLE cliente(
 CREATE TABLE gerente(
    id_gerente INT NOT NULL auto_increment,
    nome_gerente VARCHAR(45) NOT NULL,
-   fk_bairro_id VARCHAR(45) NOT NULL,
+   fk_bairro_id INT NOT NULL,
    PRIMARY KEY(id_gerente),
    FOREIGN KEY (fk_bairro_id) REFERENCES bairro(id_bairro)
    ) ENGINE = innoDB DEFAULT CHARSET=utf8;
